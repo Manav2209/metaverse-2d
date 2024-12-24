cf31616aa1733e8cf2656342120f7d4b6935f994
Manav2209 <manavpanchal2854@gmail.com>
2024-12-24 08:04:53 +0530

    Web Socet completed

--- metaverse/apps/ws/src/index.ts
+++ metaverse/apps/ws/src/index.ts
@@ -4,8 +4,9 @@ import { User } from './User';
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+ 
   let user = new User(ws);
+  ws.on('error' , console.error);
 
   ws.on('close', () =>{
     user?.destroy();
